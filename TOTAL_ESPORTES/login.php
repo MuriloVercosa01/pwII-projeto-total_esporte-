01f6da80eb7f277041482b356ea8b8c231e7f6ac
JOAO PEDRO XAVIER NASCIMENTO <JOAO_4602@uirapuru.br>
2024-11-25 21:46:23 -0300

    adicionando footer para as páginas

--- TOTAL_ESPORTES/login.php
+++ TOTAL_ESPORTES/login.php
@@ -51,10 +51,64 @@
             </div>
             <button type="submit" class="btn_login" value="Enviar" name="submit" > Entrar </button>
             <p id="redc_cadastro">Ainda não possui cadastro? <a href="cadastro.php" >Cadastre-se</a></p>  
-</form>
-    </main>
+        </form>
+    </main >
 
+    <footer>
+        <!-- Container principal -->
+    <div id="conteudo-rodape">
+    <!-- Contatos e redes sociais -->
+        <div id="contatos-rodape">
+            <img src="imagens/Logo.png" alt="Logo Total Esporte">
+            <p>Nossas redes sociais</p>
+            <div id="redes-sociais-rodape">
+            <a href="#" class="footer-link" id="instagram">
+            <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#" class="footer-link" id="facebook">
+            <i class="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" class="footer-link" id="whatsapp">
+            <i class="fa-brands fa-whatsapp"></i>
+            </a>
+      </div>
+    </div>
 
+    <!-- Listas de serviços e produtos -->
+    <ul class="lista-rodape">
+      <li><h3>Serviços</h3></li>
+      <li><p>Entrega grátis a partir de R$ 200,00</p></li>
+      <li><p>30 dias para trocas e devoluções</p></li>
+      <li><p>Atendimento online 24 horas</p></li>
+    </ul>
+    <ul class="lista-rodape">
+      <li><h3>Produtos</h3></li>
+      <li><p>Chuteira Futsal</p></li>
+      <li><p>Chuteira Society</p></li>
+      <li><p>Chuteira Campo</p></li>
+    </ul>
+
+    <!-- Suporte online -->
+    <div id="suporte-rodape">
+      <h3>Suporte Online</h3>
+      <p>
+        A Total Esporte oferece suporte online pelo e-mail suporte@totalesporte.com.
+        Nossa equipe está pronta para ajudá-lo!
+      </p>
+      <div id="email-suporte">
+        <a>
+          <i class="fa-regular fa-envelope"></i>
+        </a>
+        <h1>suporte@totalesporte.com</h1>
+      </div>
+    </div>
+  </div>
+
+  <!-- Direitos autorais -->
+  <div id="direitos-autorais-rodape">
+    &#169; 2024 Total Esporte
+  </div>
+    </footer>
     
 </body>
 </html>

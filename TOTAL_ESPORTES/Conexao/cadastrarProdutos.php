eacd7091c206ff6e22d9d2a0b56535c282b30383
bunnur <murilomiransa5@gmail.com>
2024-12-07 16:03:28 -0300

    comecei a fazer categoria/subcategoria

--- TOTAL_ESPORTES/Conexao/cadastrarProdutos.php
+++ TOTAL_ESPORTES/Conexao/cadastrarProdutos.php
@@ -1,6 +1,4 @@
 <?php
-    print_r($_REQUEST);
-    echo"</br> subcategoria: " . $_REQUEST['subcategoria'];
     if( !empty($_POST['img']) && !empty($_POST['modelo']) && !empty($_POST['categoria']) && !empty($_POST['subcategoria'])&& !empty($_POST['desc_breve']) && !empty($_POST['valor']) ){
         // caso todos os dados tenham sido enviados
         $_img = $_POST['img'];
@@ -12,6 +10,10 @@
         
         include_once('conexao.php');
 
+        if ($conexao->connect_error){
+            die('a conexão falhou:' . $conexao->connect_error);
+        }
+
         $sql = "SELECT * FROM sub_categoria WHERE id_sub_categoria = ? AND id_categoria = ?;";
 
         $stmt = $conexao->prepare($sql);
@@ -22,7 +24,7 @@
 
         $result = $stmt->get_result();
 
-        if($result->num_rows < 0){
+        if($result->num_rows === 0){
             echo' <script>
                     alert("subcategoria relacionada de forma incorreta a categoria");
 
@@ -32,29 +34,25 @@
                         window.location.href = url;
                     } 
                   </script> ';
+        }else{
+            $sql = 'INSERT INTO produto (imagem,modelo,id_categoria,id_subcategoria,desc_breve,preco) VALUES (?,?,?,?,?,?)';
+
+            $stmt = $conexao->prepare($sql);
+    
+            $stmt->bind_param("sssssd",$_img,$_modelo,$_categoria,$_subcategoria,$_desc,$_valor);
+    
+            $stmt->execute();
+            //dados inseridos no banco de dados
+            echo('<script>
+                    alert("produto cadastrado com sucesso");
+    
+                    redirecionar("http://localhost/pwII-projeto-total_esporte-/TOTAL_ESPORTES/addprodutos.php");
+    
+                    function redirecionar(url){
+                        window.location.href = url;
+                    } 
+                </script>');
         }
-        //se der pau na conexão =)
-        if ($conexao->connect_error){
-            die('a conexão falhou:' . $conexao->connect_error);
-        }
-
-        $sql = 'INSERT INTO produto (imagem,modelo,id_categoria,id_subcategoria,desc_breve,preco) VALUES (?,?,?,?,?,?)';
-
-        $stmt = $conexao->prepare($sql);
-
-        $stmt->bind_param("sssssd",$_img,$_modelo,$_categoria,$_subcategoria,$_desc,$_valor);
-
-        $stmt->execute();
-        //dados inseridos no banco de dados
-        echo('<script>
-                alert("produto cadastrado com sucesso");
-
-                redirecionar("http://localhost/pwII-projeto-total_esporte-/TOTAL_ESPORTES/addprodutos.php");
-
-                function redirecionar(url){
-                    window.location.href = url;
-                } 
-            </script>');
     } else{
         //caso algum campo não tenha sido preenchido
         echo('há campos não preenchidos');
